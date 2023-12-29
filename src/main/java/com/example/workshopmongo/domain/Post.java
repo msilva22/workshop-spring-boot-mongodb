7af71437affe2ad164aea74a507f4ba2ad251dd3
Moisés Silva <moises.ambiental@hotmail.com>
2023-12-29 16:10:40 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/example/workshopmongo/domain/Post.java
+++ src/main/java/com/example/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.example.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.workshopmongo.dto.AuthorDTO;
+import com.example.workshopmongo.dto.CommentDTO;
 
 
 @Document
@@ -21,6 +24,9 @@ public class Post  implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	
 	public Post() {}
 
@@ -76,7 +82,18 @@ public class Post  implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+		
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+
 
 	@Override
 	public int hashCode() {
@@ -93,8 +110,6 @@ public class Post  implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
-
 
 	
 }
